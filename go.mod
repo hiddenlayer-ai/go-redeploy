4fd481a51735fb19fbc63b11d1afc60d07c33ed9
James Taylor <jamestaylor3@protonmail.com>
2020-07-24 01:01:45 -0400

    config file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hiddenlayer-ai/go-redeploy
 
 go 1.13
+
+require github.com/BurntSushi/toml v0.3.1
--- redeploy.go
+++ redeploy.go
@@ -7,33 +7,34 @@ import (
 	"os/exec"
 	"strings"
 	"time"
+
+	"github.com/BurntSushi/toml"
 )
 
-// make these accessible with a config file
-const (
-	WAITING    = 5                // time in seconds to wait before update lookup
-	REPOPATH   = "../go-redeploy" // relative path to repo to manage
-	EXECUTABLE = "go-redeploy"    // name of executable to run
-)
+type Config struct {
+	SLEEP      uint   `toml:"sleep"`
+	REPOPATH   string `toml:"repo-path"`
+	ENTRYPOINT string `toml:"entry-point"`
+}
 
 var (
 	DIFF  = "git diff origin/master"
 	RESET = "git reset --hard HEAD"
 	CLONE = "git pull --force"
 	BUILD = "go build"
-	EXEC  = fmt.Sprintf("./%s", EXECUTABLE)
+	EXEC  = fmt.Sprintf("go run %s.go", conf.ENTRYPOINT)
 )
+
+var conf Config
 
 func RUN(command string) *bytes.Buffer {
 	c := strings.Split(command, " ")
 	cmd := exec.Command(c[0], c[1:]...)
-	cmd.Dir = REPOPATH
+	cmd.Dir = conf.REPOPATH
 	out := new(bytes.Buffer)
 	cmd.Stdout = out
 
-	err := cmd.Run()
-
-	if err != nil {
+	if err := cmd.Run(); err != nil {
 		log.Print(err)
 		return nil
 	}
@@ -42,18 +43,21 @@ func RUN(command string) *bytes.Buffer {
 }
 
 func main() {
-	fmt.Println("ayyy")
+	if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
+		log.Fatal(err)
+	}
+
 	for {
 		out := RUN(DIFF)
 
-		// we found something
 		if len(strings.Split(out.String(), " ")) != 0 {
+			// new update
 			RUN(RESET)
 			RUN(CLONE)
 			RUN(BUILD)
 			RUN(EXEC)
 		}
 
-		time.Sleep(WAITING * time.Second)
+		time.Sleep(time.Duration(conf.SLEEP) * time.Second)
 	}
 }
